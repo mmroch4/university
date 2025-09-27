0c4eebf62a3a63395e01511aecea922e07ac8dc3
Miguel Rocha <miguelrocha.dev@gmail.com>
2025-09-27 21:39:43 +0100

    refactor: (IC) redefine exercise 47 execution format

--- introduction-to-programming/exercises/47/index.py
+++ introduction-to-programming/exercises/47/index.py
@@ -1,27 +1,26 @@
-r = [1, 2]
+def whichone(n):
+  r = [1, 2]
 
-n = int(input())
+  p = 2
 
-p = 2
+  while r[-1] < n:
+    r.append(r[p - 1] + r[p - 2])
+    
+    p += 1
 
-while r[-1] < n:
-  r.append(r[p - 1] + r[p - 2])
-  
-  p += 1
+  k = n
+  t = p - 1
 
-k = n
-t = p - 1
+  while t > 1:
+    left = k <= r[t - 1]
+    right = k >= r[t - 1] + 1
+    
+    if right:
+      k -= r[t - 1]
+      t -= 2
+    else:
+      t -= 1
 
-while t > 1:
-  left = k <= r[t - 1]
-  right = k >= r[t - 1] + 1
-  
-  if right:
-    k -= r[t - 1]
-    t -= 2
-  else:
-    t -= 1
+  ans = ["A", "AB"]
 
-ans = ["A", "AB"]
-
-print(ans[t][k - 1])
+  return ans[t][k - 1]
